5ad9b4cd30343858ce56d510890c7e46b3e463db
hamza72x <hamza72x@gmail.com>
2026-06-30 17:29:44 +0600

    hamza72x: suppress hyprland-guiutils warning (not packaged for Fedora yet)

--- hamza72x/configs/hypr/hyprland.lua
+++ hamza72x/configs/hypr/hyprland.lua
@@ -95,6 +95,7 @@ hl.config({
     misc = {
         force_default_wallpaper = 1,
         disable_hyprland_logo   = false,
+        disable_hyprland_guiutils_check = true,
     },
 
     ecosystem = {
